2b7fb0f9fb2428730ca425fb967de94c90caff9c
Kisakye19 <kisakyepesther@gmail.com>
2023-08-28 12:32:37 -0700

    corrections

--- 0x02-functions_nested_loops/8-main.c
+++ 0x02-functions_nested_loops/8-main.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-    jack_bauer();
-    return (0);
+jack_bauer();
+return (0);
 }
 
--- 0x02-functions_nested_loops/9-main.c
+++ 0x02-functions_nested_loops/9-main.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-    times_table();
-    return (0);
+times_table();
+return (0);
 }
 
